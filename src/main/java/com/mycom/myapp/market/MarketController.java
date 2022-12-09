b2437d9af45e8480cbcf380646396acdd5b85536
ChoiJunhyeok00 <21900764@handong.ac.kr>
2022-12-10 01:22:36 +0900

    add files

---
+++ src/main/java/com/mycom/myapp/market/MarketController.java
@@ -0,0 +1,67 @@
+package com.mycom.myapp.market;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//@RequestMapping
+@Controller
+public class MarketController {
+
+    @Autowired
+    MarketService marketService;
+
+    @RequestMapping(value="/")
+        public String home(){
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/board/list", method = RequestMethod.GET)
+    public String boardlist(Model model){
+        model.addAttribute("list",marketService.getMarketList());
+        return "list";
+    }
+
+    @RequestMapping(value="/board/add", method=RequestMethod.GET)
+    public String addPost(){
+        return "addpostform";
+    }
+
+    @RequestMapping(value="/board/addok",method=RequestMethod.POST)
+    public String addPostOK(MarketVO vo){
+        if(marketService.insertMarket(vo)==0)
+            System.out.println("데이터 추가 실패");
+        else
+            System.out.println("데이터 추가 성공!!!");
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/board/editform/{id}",method = RequestMethod.GET)
+    public String editPost(@PathVariable("id") int id, Model model) {
+        MarketVO boardVO=marketService.getMarket(id);
+        model.addAttribute("u",boardVO);
+        return "editform";
+    }
+
+    @RequestMapping(value="/board/editok",method=RequestMethod.POST)
+    public String editPostOk(MarketVO vo){
+        if(marketService.updateMarket(vo)==0)
+            System.out.println("데이터 수정 실패");
+        else
+            System.out.println("데이터 수정 성공!!!");
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/board/deleteok/{id}",method=RequestMethod.GET)
+    public String deletePostOk(@PathVariable("id") int id){
+
+        if(marketService.deleteMarket(id)==0)
+            System.out.println("데이터 삭제 실패");
+        else
+            System.out.println("데이터 삭제 성공!!!");
+        return "redirect:../list";
+    }
+}
